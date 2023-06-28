a1ea08ac84f6c938e8ca0619d3123f07323f0558
John Canero <johncanero.official@gmail.com>
2023-06-28 19:55:04 +0800

    Add Input and Button components for the AuthForm

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -52,13 +54,54 @@ const AuthForm = () => {
         setIsLoading(true);
 
         // NextAuth Social SignIn
-    } 
+    }
 
 
 
     return (
-        <div>
-            
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+                <form
+                    className="space-y-6"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    {variant === 'REGISTER' && (
+                    <Input
+                        disabled={isLoading}
+                        register={register}
+                         errors={errors}
+                        required
+                         id="name"
+                        label="Name"
+                    />
+                    )}
+                    <Input
+                        disabled={isLoading}
+                        register={register}
+                        errors={errors}
+                        required
+                        id="email"
+                        label="Email address"
+                        type="email"
+                    />
+                    <Input
+                        disabled={isLoading}
+                        register={register}
+                        errors={errors}
+                        required
+                        id="password"
+                        label="Password"
+                        type="password"
+                    />
+                    <div>
+                        <Button disabled={isLoading} fullWidth type="submit">
+                            {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+                        </Button>
+                    </div>
+                </form>
+
+                
+            </div>
         </div>
     )
 }

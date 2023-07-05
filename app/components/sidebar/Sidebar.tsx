b874d05f1d3377c03f84f6b067d3b91a08b17067
John Canero <johncanero.official@gmail.com>
2023-07-05 11:39:39 +0800

    Add Mobile Component Fiels to reflect on Tablet and Mobile

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from './MobileFooter';
 
 async function Sidebar({ children }: {
     children: React.ReactNode,
@@ -8,6 +9,7 @@ async function Sidebar({ children }: {
     return (
         <div className="h-full">
             <DesktopSidebar />
+            <MobileFooter />
             <main className="h-full lg:pl-20">
                 {children}
             </main>

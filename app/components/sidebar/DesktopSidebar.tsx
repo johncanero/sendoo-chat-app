48623f3603dcdd9878205683e54e6424474eb4ac
John Canero <johncanero.official@gmail.com>
2023-07-05 13:03:50 +0800

    Add User in Console Log

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,13 +1,21 @@
 'use client'
 
 import { useState } from "react";
+import { User } from "@prisma/client";
 import useRoutes from "@/app/hooks/useRoutes";
 import DesktopItem from "./DesktopItem";
 
+interface DesktopSidebarProps {
+    currentUser: User
+}
 
-const DesktopSidebar = () => {
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
+
+    console.log({ currentUser, }, 'TEST')
 
     return (
         <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6lg:overflow-y-auto  lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
